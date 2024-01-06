b200d6638524cdd1bef8ce946a081c2d93c2c20e
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2024-01-06 08:09:04 +0530

    Add files via upload

---
+++ MeditationTrackerApp/DAL/Interrfaces/IMeditationTrackerService.cs
@@ -0,0 +1,18 @@
+﻿using MeditationTrackerApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeditationTrackerApp.DAL.Interrfaces
+{
+    public interface IMeditationTrackerService
+    {
+        List<Meditation> GetAllMeditations();
+        Task<Meditation> CreateMeditation(Meditation meditation);
+        Task<Meditation> GetMeditationById(long id);
+        Task<bool> DeleteMeditationById(long id);
+        Task<Meditation> UpdateMeditation(Meditation model);
+    }
+}
---
+++ MeditationTrackerApp/DAL/Services/MeditationTrackerService.cs
@@ -0,0 +1,46 @@
+﻿using MeditationTrackerApp.DAL.Interrfaces;
+using MeditationTrackerApp.DAL.Services.Repository;
+using MeditationTrackerApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace MeditationTrackerApp.DAL.Services
+{
+    public class MeditationTrackerService : IMeditationTrackerService
+    {
+        private readonly IMeditationTrackerRepository _repository;
+
+        public MeditationTrackerService(IMeditationTrackerRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public Task<Meditation> CreateMeditation(Meditation expense)
+        {
+            return _repository.CreateMeditation(expense);
+        }
+
+        public Task<bool> DeleteMeditationById(long id)
+        {
+            return _repository.DeleteMeditationById(id);
+        }
+
+        public List<Meditation> GetAllMeditations()
+        {
+            return _repository.GetAllMeditations();
+        }
+
+        public Task<Meditation> GetMeditationById(long id)
+        {
+            return _repository.GetMeditationById(id); ;
+        }
+
+        public Task<Meditation> UpdateMeditation(Meditation model)
+        {
+            return _repository.UpdateMeditation(model);
+        }
+    }
+}
